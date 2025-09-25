f08c1c0d873e10b31b47232a5fedaefa967a4970
alexandree11 <analexandr.99@gmail.com>
2025-09-24 22:57:50 -0400

    comments upd

--- 3320/hw1/pcbqueue_hw.cpp
+++ 3320/hw1/pcbqueue_hw.cpp
@@ -27,8 +27,8 @@ struct PCB{
 // PCB Queue class
 class PCBQueue{
     private:
-        PCB queue[MAX_PCB_SIZE]; // array to hold PCBs
-        int first, last, count; // first and last index, count of PCBs in queue
+        PCB queue[MAX_PCB_SIZE]; // array to hold MAX_PCB_SIZE amount of processes in queue
+        int first, last, count; // first and last index, count of processes in queue
 
     public:
         PCBQueue(){
@@ -54,7 +54,7 @@ class PCBQueue{
                      << "Priority: " << pcb.priority << endl << endl;
                 return;
             }
-            last = (last + 1) % MAX_PCB_SIZE; // circular list (if last becomes MAX then it goes to 0)
+            last = (last + 1) % MAX_PCB_SIZE; // extend the tail in circular list (if last becomes MAX then it goes to 0)
             queue[last] = pcb; // add pcd to the queue at new last
             count++; // increment to compare with isEmpty and isFull
         }
@@ -101,11 +101,15 @@ int main(){
 
     PCB filePCB;
     //loop to read from a file and assign values to filePCB(pid, status, priority)
+    //then add the PCB object to the queue
+    //repeat while end of the file is not reached and print the queue at the end of the loop
     while(infile >> filePCB.pid >> filePCB.status >> filePCB.priority){
         pcbQueue.add(filePCB);
     }
     infile.close();
     pcbQueue.printQueue();
+
+    //removing first three added processes
     pcbQueue.remove();
     pcbQueue.remove();
     pcbQueue.remove();
@@ -117,11 +121,14 @@ int main(){
     pcbQueue.add(newPCB);
     newPCB = {12999, "BLOCKED", 9};
     pcbQueue.add(newPCB);
+
+    //this process should not be added since its already 10 processes in the queue
     newPCB = {13000, "READY", 1};
     pcbQueue.add(newPCB);
+
     pcbQueue.printQueue();
 
-    //to clear the queue
+    //to clear the queue (this could be a function)
     while(!pcbQueue.isEmpty()){
         pcbQueue.remove();
     }

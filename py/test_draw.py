6114ed8d02ac056036679542d38f1174e63989c4
alexandree11 <analexandr.99@gmail.com>
2026-09-19 06:34:13 -0400

    Add initial drawing functions using turtle graphics

---
+++ py/test_draw.py
@@ -0,0 +1,28 @@
+from turtle import *
+
+def draw_rombs():
+    speed(1)
+    while(True):
+        penup()
+        goto(0,0)
+        pendown()
+        goto(40,40)
+        goto(120,-40)
+        goto(160,0)
+        goto(120,40)
+        goto(40,-40)
+        goto(0,0)
+        break
+
+def draw_triangles():
+
+def draw_cube():
+
+def draw_circles():
+
+def draw_compass():
+
+def draw_square():
+
+draw_rombs()
+exitonclick()
